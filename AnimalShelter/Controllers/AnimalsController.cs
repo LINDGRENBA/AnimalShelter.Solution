8ac68457c2d79e2be561a7de96834709d39aeb20
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-28 13:46:13 -0700

    add edit and delete functionality
    Co-authored-by: Erich Richter <erich.richter@gmail.com>
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -1,8 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Controllers
 {
@@ -30,6 +30,33 @@ namespace AnimalShelter.Controllers
     public ActionResult Create(Animal animal)
     {
       _db.Animals.Add(animal);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+      return View(thisAnimal);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Animal animal)
+    {
+      _db.Entry(animal).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+      return View(thisAnimal);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+      _db.Animals.Remove(thisAnimal);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
